bc2ba97e5f6d20360590f1aaf610b421d2f2ad22
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-06-27 18:12:17 +0200

    moved ecs to ec2 host (#4)

---
+++ infrastructure/modules/ec2.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "ec2_instance" {
+  ami                    = "ami-07480655a02fc53ae" #Amazon ECS-Optimized Amazon Linux 2 x86_64 AMI
+  subnet_id              = module.vpc.private_subnets[0]
+  instance_type          = var.instance_type
+  iam_instance_profile   = aws_iam_instance_profile.ecs_agent.name
+  vpc_security_group_ids = [aws_security_group.ec2_ecs_instance.id]
+  ebs_optimized          = "false"
+  source_dest_check      = "false"
+  tags                   = {
+    Name = "${var.app_name}-api-${var.environment}"
+  }
+  user_data              = <<EOF
+    #!/bin/bash
+
+    # sudo amazon-linux-extras disable docker
+    # sudo amazon-linux-extras install -y ecs; 
+    echo ECS_CLUSTER=ecs-${var.app_name}-api-${var.environment} >> /etc/ecs/ecs.config
+    # cat /etc/ecs/ecs.config | grep "ECS_CLUSTER"
+    # sudo systemctl enable --now ecs
+  EOF
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = "30"
+    delete_on_termination = "true"
+    tags = {
+      Name = "${var.app_name}-api-${var.environment}"
+    }
+  }
+}
--- infrastructure/modules/variables.tf
+++ infrastructure/modules/variables.tf
@@ -1,5 +1,47 @@
-variable "environment" {}
+## General Variables ###
+variable "domain_name" {
+  type        = string
+  description = "The domain name"
+  default     = "pw-pusher.click"
+}
+
+variable "r53_zone_id" {
+  type        = string
+  description = "The zone ID of the domain"
+  default     = "Z0718599212MVN64WNQI1"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Define the EC2 Instance type for the ecs cluster"
+  default     = "t3.micro"
+}
+
+variable "container_image" {
+  type        = string
+  description = "Docker image for ECS task"
+}
+
+variable "app_name" {
+  type = string
+  description = "Application name"
+  default = "pw-pusher"
+}
+
+variable "environment" {
+  type = string
+  description = "Environment"
+  default = "dev"
+}
+
+variable "task_count" {
+  type = number
+  description = "Number of running ECS tasks"
+  default = 1
+}
+
 variable "region" {
+  type = string
+  description = "Region"
   default = "eu-central-1"
 }
-variable "container_tag" {}
