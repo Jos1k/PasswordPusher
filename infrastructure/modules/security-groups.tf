28dae1e904819f6c4345ee875d9a534012c4956b
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-07-02 11:03:44 +0200

    moved out of private subnet (#9)

--- infrastructure/modules/security-groups.tf
+++ infrastructure/modules/security-groups.tf
@@ -35,31 +35,33 @@ resource "aws_security_group_rule" "public_in_https" {
   security_group_id = aws_security_group.public.id
 }
 
-resource "aws_security_group" "ec2_ecs_instance" {
-  name        = "Allow internal VPC traffic"
-  description = "Allow internal VPC traffic"
+
+
+
+resource "aws_security_group" "ec2" {
+  name        = "Allow traffic from ALB"
+  description = "Allow traffic from ALB"
   vpc_id      = module.vpc.vpc_id
-  
   tags                   = {
-    Name = "${var.app_name}-${var.environment}-private-sg"
+    Name = "${var.app_name}-${var.environment}-ec2-sg"
   }
 }
 
-resource "aws_security_group_rule" "allow_internal_VPC_traffic" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["10.99.0.0/18"]
-  security_group_id = aws_security_group.ec2_ecs_instance.id
-}
-
-resource "aws_security_group_rule" "public_out_ec2" {
+resource "aws_security_group_rule" "ec2_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group.ec2_ecs_instance.id
+  security_group_id = aws_security_group.ec2.id
+}
+
+resource "aws_security_group_rule" "ec2_in_http" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  source_security_group_id = aws_security_group.public.id
+  security_group_id = aws_security_group.ec2.id
 }
