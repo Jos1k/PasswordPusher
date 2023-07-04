7328f0bc7ab279612d1abcc5cda1cc4343531e0e
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-07-04 14:24:03 +0200

    added prod terraform

---
+++ infrastructure/prod/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  backend "s3" {
+    bucket = "terraformpasswordpusherstate"
+    key    = "state.tfstate"
+    region = "eu-central-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.5.0"
+    }
+  }
+
+  required_version = "= 1.5.1"
+}
+
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+module "main" {
+  source          = "../modules"
+  container_image = var.container_image
+  environment     = "prod"
+  domain_name     = var.domain_name
+  r53_zone_id     = var.r53_zone_id
+}
+
+variable "container_image" {
+  type        = string
+  description = "Docker image for ECS task"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "Domain name"
+}
+
+variable "r53_zone_id" {
+  type        = string
+  description = "Route 53 Hosted Zone Id"
+}
