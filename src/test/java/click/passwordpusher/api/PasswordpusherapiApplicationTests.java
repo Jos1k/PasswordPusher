7f411b3e62fe244ffef51fa2d87ba1501ceaa90c
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-07-06 08:50:15 +0200

    added java hello world

---
+++ src/test/java/click/passwordpusher/api/PasswordpusherapiApplicationTests.java
@@ -0,0 +1,13 @@
+package click.passwordpusher.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordpusherapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
