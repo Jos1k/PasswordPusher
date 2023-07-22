1dcb2d4406c9c708121f32107b202744c326baee
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-07-22 09:09:58 +0200

    fix retain env

--- infrastructure/modules/logs.tf
+++ infrastructure/modules/logs.tf
@@ -1,6 +1,8 @@
 module "solutions_cloudwatch-log-retention-manager" {
   source                            = "terraform-aws-modules/solutions/aws//modules/cloudwatch-log-retention-manager"
+  cloudwatch_logs_retention_in_days = 1
   tags                              = { Name = "${var.app_name}-api-${var.environment}" }
   putin_khuylo                      = true
   schedule_expression               = "rate(24 hours)"
+  environment_variables             = { RETAIN_DAYS = var.logs_retention_in_days }
 }
