7a8c6ea996e6da2fca6ea20438462fa539f5d00d
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-07-22 08:46:51 +0200

    Add log retention manager

---
+++ infrastructure/modules/logs.tf
@@ -0,0 +1,7 @@
+module "solutions_cloudwatch-log-retention-manager" {
+  source                            = "terraform-aws-modules/solutions/aws//modules/cloudwatch-log-retention-manager"
+  cloudwatch_logs_retention_in_days = var.logs_retention_in_days
+  tags                              = { Name = "${var.app_name}-api-${var.environment}" }
+  memory_size                       = 128
+  putin_khuylo                      = true
+}
