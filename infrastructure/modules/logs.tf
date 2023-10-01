43e489cae83b683e1bb6f1bf3e48ae2050c54d12
Ihor Hadzera <ihor.hadzera@gmail.com>
2023-10-01 15:51:29 +0200

    fix_logs

--- infrastructure/modules/logs.tf
+++ infrastructure/modules/logs.tf
@@ -1,5 +1,5 @@
 module "solutions_cloudwatch-log-retention-manager" {
-  source                            = "terraform-aws-modules/solutions/aws//modules/cloudwatch-log-retention-manager"
+  source                            = "terraform-aws-modules/solutions/aws/modules/cloudwatch-log-retention-manager"
   cloudwatch_logs_retention_in_days = 1
   tags                              = { Name = "${var.app_name}-api-${var.environment}" }
   putin_khuylo                      = true
